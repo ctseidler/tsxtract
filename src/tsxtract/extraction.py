34ab976f2c9c60629587f72719ca61ab575deb22
Christian Seidler <139808228+ctseidler@users.noreply.github.com>
2025-08-05 20:45:50 +0200

    Increased computational speed using jit compilation and flattening nested vmap call

--- src/tsxtract/extraction.py
+++ src/tsxtract/extraction.py
@@ -10,6 +10,9 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from collections.abc import Callable
 
+
+from functools import partial
+
 import jax
 
 from tsxtract.configuration import ExtractionConfiguration
@@ -17,6 +20,7 @@ from tsxtract.utils import measure_runtime
 
 
 @measure_runtime()
+@partial(jax.jit, static_argnames=["config"])
 def extract_features(dataset: jax.Array, config: ExtractionConfiguration) -> dict[str, jax.Array]:
     """Extract a bunch of features.
 
@@ -27,6 +31,14 @@ def extract_features(dataset: jax.Array, config: ExtractionConfiguration) -> dic
     feature_extractors: dict[str, Callable] = config.map_settings_to_feature_extractors()
 
     for name, extractor in feature_extractors.items():
-        extracted_features[name] = jax.vmap(jax.vmap(extractor))(dataset)
+        extracted_features[name] = flat_vmap(extractor, dataset)
 
     return extracted_features
+
+
+def flat_vmap(fn, x):
+    """Applying vmap on (samples, channels) simultaneously."""
+    s, c, t = x.shape
+    x_flat = x.reshape(s * c, t)
+    out = jax.vmap(fn)(x_flat)
+    return out.reshape(s, c, *out.shape[1:])
